66f4e08b5caab0113bd49a6c8f17a584fe789efe
Rochak Pandey <rochakpandey56@gmail.com>
2023-01-20 16:45:17 +0530

    "Initial project setup with Typescript and Flowbite"

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App bg-green-800 w-screen h-screen">
+      <h1 className="text-4xl text-center text-green-200 ">
+        Hi there, project setup successfully
+      </h1>
+    </div>
+  );
+}
+
+export default App;
