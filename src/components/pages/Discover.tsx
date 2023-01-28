a0b34a2d78f8f5e17f0a53518a7457c67cfbe66a
Rochak Pandey <rochakpandey56@gmail.com>
2023-01-28 13:22:49 +0530

    Updated Pagination to persist state 🥳

--- src/components/pages/Discover.tsx
+++ src/components/pages/Discover.tsx
@@ -1,24 +1,39 @@
 import React, { useEffect, useState } from "react";
 import { DiscoverFeed, Pagination } from "../exports";
-import { useParams } from "react-router-dom";
+import { useLocation, useNavigate, useParams } from "react-router-dom";
 import { getDiscover } from "../../utils/fetchFromAPI";
+
+const useQuery = () => {
+  const { search } = useLocation();
+  return React.useMemo(() => new URLSearchParams(search), [search]);
+};
+
 const Discover = () => {
+  const navigate = useNavigate();
+  let query = useQuery();
+  const currentPage = query.get("page") ? parseInt(query.get("page")!) : 1;
+
   const { mediaType } = useParams();
-  const [currentPage, setcurrentPage] = useState(1);
+
+  // states for the pagination
   const [totalPages, settotalPages] = useState(1);
+  const [discoverMedia, setDiscoverMedia] = React.useState([]);
   const [pagesArray, setpagesArray] = useState(Array<number>);
 
+  // handler for the pagination
+  const setCurrentPageHandler = (pageNumber: number) => {
+    navigate({
+      pathname: `/discover/${mediaType}`,
+      search: `?page=${pageNumber}`,
+    });
+  };
+  // useEffect for the pagination
   useEffect(() => {
     const pages = Array.from(Array(totalPages).keys(), (n) => n + 1);
     setpagesArray(pages);
   }, [totalPages]);
 
-  const setCurrentPageHandler = (pageNumber: number) => {
-    setcurrentPage(pageNumber);
-  };
-
-  const [discoverMedia, setDiscoverMedia] = React.useState([]);
-  React.useEffect(() => {
+  useEffect(() => {
     settotalPages(20);
     window.scrollTo({
       top: 0,
@@ -30,6 +45,7 @@ const Discover = () => {
       else setDiscoverMedia(data);
     });
   }, [mediaType, currentPage]);
+
   return (
     <>
       <DiscoverFeed mediaInfo={discoverMedia} mediaType={mediaType!} />
