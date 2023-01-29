756b388a128de3d7ce18cf8776493a0d6e430632
Rochak Pandey <rochakpandey56@gmail.com>
2023-01-29 20:12:20 +0530

    Added lastupdate time in localstorage | Removed trailer from upcoming movies. it's not provided

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,20 @@
+import { useEffect } from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
 import { Navbar, Home, Details, Discover, Footer } from "./components/exports";
 
 function App() {
+  useEffect(() => {
+    const dataLastUpdated = localStorage.getItem("dataLastUpdated");
+    if (dataLastUpdated) {
+      const timeDiff = new Date().getTime() - parseInt(dataLastUpdated);
+      if (timeDiff > 3600000) {
+        localStorage.clear();
+      }
+    } else {
+      localStorage.setItem("dataLastUpdated", new Date().getTime().toString());
+    }
+  });
   return (
     <div className="App w-screen scroll-smooth bg-gray-900 text-white">
       <BrowserRouter>
--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,12 +1,7 @@
 import { LanguageIcon } from "@heroicons/react/20/solid";
-import {
-  InformationCircleIcon,
-  PlayCircleIcon,
-} from "@heroicons/react/24/outline";
-import { useEffect, useState } from "react";
+import { InformationCircleIcon } from "@heroicons/react/24/outline";
+import { useState } from "react";
 import { Link } from "react-router-dom";
-import { youtubeBaseUrl } from "../utils/constants";
-import { getTrailer } from "../utils/fetchFromAPI";
 import {
   formatDate,
   getGenreList,
@@ -17,14 +12,6 @@ interface MainProps {
   movieInfo: any;
 }
 const Main = (props: MainProps) => {
-  const [trailer, setTrailer] = useState("");
-  const [trailerModalOpen, setTrailerModalOpen] = useState(false);
-  useEffect(() => {
-    getTrailer("movie", movieInfo.id).then((data) => {
-      // const key = data.results.find((item: any) => item.type === "Trailer").key;
-      setTrailer(data);
-    });
-  });
   // destructuring props
   const { movieInfo } = props;
 
@@ -37,30 +24,30 @@ const Main = (props: MainProps) => {
   // function to format date in dd Month format
 
   return (
-    <div className="flex flex-col h-full translate-y-1/4 px-4 lg:px-24">
-      <div className="text-sm lg:text-xl font-light">{genreList}</div>
-      <h1 className="text-4xl sm:text-5xl md:7xl font-bold uppercase">
+    <div className="flex h-full translate-y-1/4 flex-col px-4 lg:px-24">
+      <div className="text-sm font-light lg:text-xl">{genreList}</div>
+      <h1 className="md:7xl text-4xl font-bold uppercase sm:text-5xl">
         {movieInfo.title}
       </h1>
       <div className="flex gap-2">
         <LanguageIcon className="h-6 w-6 text-primary-yellow" />
-        <div className="text-sm lg:text-xl font-light">{originalLanguage}</div>
+        <div className="text-sm font-light lg:text-xl">{originalLanguage}</div>
       </div>
-      <div className="text-2xl md:text-3xl mt-8">
+      <div className="mt-8 text-2xl md:text-3xl">
         In cinemas on{" "}
         <span className="5xl font-semibold text-gray-100">
           {formatDate(movieInfo.release_date)}
         </span>{" "}
       </div>
       <div className="flex items-center gap-4">
-        {trailer && (
+        {/* {trailer && (
           <PrimaryBtn
             title="Watch Trailer"
             onClickHandler={() => {}}
             iconComponent={<PlayCircleIcon className="w-6" />}
             classList="mt-8 transition-transform transform hover:scale-110"
           />
-        )}
+        )} */}
         <Link to={`details/movie/${movieInfo.id}`}>
           <SecondaryBtn
             title="Learn More"
@@ -70,14 +57,14 @@ const Main = (props: MainProps) => {
           />
         </Link>
       </div>
-      <Modal setIsOpen={setTrailerModalOpen} isOpen={trailerModalOpen}>
+      {/* <Modal setIsOpen={setTrailerModalOpen} isOpen={trailerModalOpen}>
         <iframe
           height="500"
           src={`${youtubeBaseUrl}${trailer}`}
           title={movieInfo.title}
           allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
         ></iframe>
-      </Modal>
+      </Modal> */}
     </div>
   );
 };
